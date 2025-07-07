2a993b2c218a15e4d6d0636f83f24fd65cb5cf90
Lisa <liza.shoshkina@gmail.com>
2025-07-07 09:50:13 +0200

    Refine types

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,25 +2,31 @@ import * as React from "react";
 
 import "./button.scss";
 
+type ButtonVariant = "primary" | "confirm" | "danger";
+
 type ButtonProps = Omit<
   React.ButtonHTMLAttributes<HTMLButtonElement>,
   "type"
 > & {
   type?: "button" | "submit" | "reset";
   label: string;
-  variant?: "primary" | "confirm" | "danger";
+  variant?: ButtonVariant;
 };
 
 const Button: React.FC<ButtonProps> = ({
   type = "button",
   label,
   variant = "primary",
+  className,
   ...buttonProps
 }) => {
-  const classNames = ["button", `button-${variant}`];
   return (
-    <button {...buttonProps} type={type} className={classNames.join(" ")}>
-      {label && <span>{label}</span>}
+    <button
+      {...buttonProps}
+      type={type}
+      className={`button button-${variant}${className ? ` ${className}` : ""}`}
+    >
+      {label}
     </button>
   );
 };
